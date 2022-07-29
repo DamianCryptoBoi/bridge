c8c2ed7bf24f55ad6bd6569a6a6756c0c0294a34
Damian-Nguyen <damian@label.foundation>
2022-07-29 10:56:10 +0700

    ...

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -6,8 +6,21 @@ import "./IERC20.sol";
 import "./Ownable.sol";
 
 contract Bridge is SignatureVerifier, Ownable {
+    uint256 public requiredRelayerToApprove;
+    uint256 public depositFee; // fixed number in native currency
+    uint256 public transferFee; // % in swap token
+    uint256 public transferFeeDenominator = 10000;
+
     mapping(address => uint256) private _userNonce;
     mapping(address => address) private _matchPair;
+
+    mapping(address => bool) public relayer;
+
+    mapping(address => bool) public whitelist; // no fees
+    mapping(address => bool) public blacklist; // blocked
+
+    mapping(address => mapping(uint256 => uint256)) public transferApprovals;
+
     event TokenMovedToBridge(
         address indexed from,
         address tokenAddress,
@@ -22,6 +35,63 @@ contract Bridge is SignatureVerifier, Ownable {
         uint256 nonce,
         bytes signature
     );
+
+    constructor(
+        uint256 _requiredRelayerToApprove,
+        address[] memory _relayers,
+        address[] memory _whitelist,
+        uint256 _depositFee,
+        uint256 _transferFee
+    ) {
+        requiredRelayerToApprove = _requiredRelayerToApprove;
+        depositFee = _depositFee;
+        transferFee = _transferFee;
+        _setRelayer(_relayers, true);
+        _setWhitelist(_whitelist, true);
+    }
+
+    function setRelayer(address[] memory _relayers, bool _authorized)
+        external
+        onlyOwner
+    {
+        _setRelayer(_relayers, _authorized);
+    }
+
+    function _setRelayer(address[] memory _relayers, bool _authorized)
+        internal
+    {
+        for (uint8 i = 0; i < _relayers.length; i++) {
+            relayer[_relayers[i]] = _authorized;
+        }
+    }
+
+    function setWhitelist(address[] memory _whitelist, bool _isTrue)
+        external
+        onlyOwner
+    {
+        _setWhitelist(_whitelist, _isTrue);
+    }
+
+    function _setWhitelist(address[] memory _whitelist, bool _isTrue) internal {
+        for (uint8 i = 0; i < _whitelist.length; i++) {
+            whitelist[_whitelist[i]] = _isTrue;
+        }
+    }
+
+    function setBlacklist(address[] memory _blacklist, bool _isTrue)
+        external
+        onlyOwner
+    {
+        _setBlacklist(_blacklist, _isTrue);
+    }
+
+    function _setBlacklist(address[] memory _blacklist, bool _isTrue) internal {
+        for (uint8 i = 0; i < _blacklist.length; i++) {
+            blacklist[_blacklist[i]] = _isTrue;
+        }
+    }
+
+    function _approveTransfer() internal {}
 
     function getUserNonce() public view returns (uint256) {
         return _userNonce[msg.sender];
